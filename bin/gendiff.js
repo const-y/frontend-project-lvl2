4f2dbb49f337080a56a95a2f2a1ea6112d88df54
Konstantin Yarish <pm-mc@mail.ru>
2021-02-27 23:05:52 +0300

    Read data from package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('I am ready');
+import { readFileSync } from 'fs';
+import program from 'commander';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const packageData = readFileSync(join(__dirname, '..', '/package.json'));
+const { version, description } = JSON.parse(packageData);
+
+program
+    .version(version)
+    .description(description)
+
+program.parse();
