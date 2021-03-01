c294537fd04e91a010a1b9e78b0f2ddd2b3bc958
Konstantin Yarish <pm-mc@mail.ru>
2021-03-02 00:42:53 +0300

    Add test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,8 +2,26 @@ import { it, expect } from '@jest/globals';
 import genDiff from '../src/genDiff';
 
 it('should compare correctly', () => {
-  const value1 = {};
-  const value2 = {};
+  const value1 = {
+    host: 'hexlet.io',
+    timeout: 50,
+    proxy: '123.234.53.22',
+    follow: false,
+  };
+  const value2 = {
+    timeout: 20,
+    verbose: true,
+    host: 'hexlet.io',
+  };
   const result = genDiff(value1, value2);
-  expect(result).toEqual({});
+  expect(result).toBe(`
+  {
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }
+  `);
 });
