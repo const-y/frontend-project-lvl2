afce71fc8fa9b6fcf7138af1d0086d6af5bd5c4f
Konstantin Iarish <konstantin.yarish@gmail.com>
2021-03-12 00:18:51 +0300

    Add working with files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 import program from 'commander';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import genDiff from '../src/genDiff.js';
 
 const filename = fileURLToPath(import.meta.url);
 
@@ -16,7 +17,20 @@ program
   .version(version)
   .arguments('<filepath1> <filepath2>')
   .description(description)
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    try {
+      const data1 = JSON.parse(readFileSync(filepath1));
+      const data2 = JSON.parse(readFileSync(filepath2));
+      console.log(genDiff(data1, data2));
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.error(`File '${error.path}' not found`);
+      } else {
+        console.error(error);
+      }
+    }
+  });
 
 program.parse();
 
