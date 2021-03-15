709e2f0d269248b8d7580e977bf6f8a157aecf80
Konstantin Iarish <konstantin.yarish@gmail.com>
2021-03-15 20:59:53 +0300

    Refactor compareByKey

--- src/genDiff.js
+++ src/genDiff.js
@@ -5,22 +5,22 @@ const prefixes = {
 };
 
 const compareByKey = (value1, value2, key) => {
-  if (value1[key] === value2[key]) {
+  if (value1 === value2) {
     // not changed
-    return [`${prefixes.nothing} ${key}: ${value1[key]}`];
+    return [`${prefixes.nothing} ${key}: ${value1}`];
   }
-  if (!(key in value1)) {
+  if (value1 === undefined) {
     // value was added
-    return [`${prefixes.added} ${key}: ${value2[key]}`];
+    return [`${prefixes.added} ${key}: ${value2}`];
   }
-  if (!(key in value2)) {
+  if (value2 === undefined) {
     // value was removed
-    return [`${prefixes.removed} ${key}: ${value1[key]}`];
+    return [`${prefixes.removed} ${key}: ${value1}`];
   }
   // both values is set - changed
   return [
-    `${prefixes.removed} ${key}: ${value1[key]}`,
-    `${prefixes.added} ${key}: ${value2[key]}`,
+    `${prefixes.removed} ${key}: ${value1}`,
+    `${prefixes.added} ${key}: ${value2}`,
   ];
 };
 
@@ -38,7 +38,7 @@ export default (value1, value2) => {
   const sortedKeys = [...new Set([...Object.keys(value1), ...Object.keys(value2)])].sort();
 
   sortedKeys.forEach((key) => {
-    diffs.push(...compareByKey(value1, value2, key));
+    diffs.push(...compareByKey(value1[key], value2[key], key));
   });
 
   return formatDiffs(diffs);
