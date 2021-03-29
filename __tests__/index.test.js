5e53245289b09b06ea0dc178a4ae2b5d8416f614
Konstantin Iarish <konstantin.yarish@gmail.com>
2021-03-29 22:21:49 +0300

    Add tests for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,27 @@
-import { expect, it } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import path from 'path';
 import { __dirname } from '../src/projectPath';
 import genDiff from '../index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-it('should compare files correctly', () => {
-  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(result).toBe(`{
+describe('genDiff()', () => {
+  const expected = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`);
+}`;
+
+  test('comparing flat json files', () => {
+    const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+    expect(result).toBe(expected);
+  });
+
+  test('comparing flat yml files', () => {
+    const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+    expect(result).toBe(expected);
+  });
 });
