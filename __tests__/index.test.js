491d2bb105cc7092e3c8426c5531452164947f8b
Konstantin Iarish <konstantin.yarish@gmail.com>
2021-03-29 23:33:50 +0300

    Revert refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,6 @@ import { __dirname } from '../src/projectPath';
 import genDiff from '../index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const getFixturePaths = (...filePaths) => filePaths.map((filepath) => getFixturePath(filepath));
 
 describe('genDiff()', () => {
   const expected = `{
@@ -17,12 +16,12 @@ describe('genDiff()', () => {
 }`;
 
   test('comparing flat json files', () => {
-    const result = genDiff(...getFixturePaths('file1.json', 'file2.json'));
+    const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
     expect(result).toBe(expected);
   });
 
   test('comparing flat yml files', () => {
-    const result = genDiff(...getFixturePaths('file1.yml', 'file2.yml'));
+    const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
     expect(result).toBe(expected);
   });
 });
